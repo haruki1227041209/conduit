6618cac4ec006dae78b2dfc71c9ea827cf3ac079
haruki1227041209 <haruki122704@gmail.com>
2024-08-31 14:59:21 +0900

    不要なファイルの削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get 'article_page/show'
   get 'article_page/new'
   get 'article_page/edit'
-  get 'home_page/index'
-  root "home_page#index"
+  root "article_page#index"
 end
