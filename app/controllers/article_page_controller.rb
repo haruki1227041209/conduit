8574b299dcd9b7ab8d270af161283ca4aa825067
haruki1227041209 <haruki122704@gmail.com>
2024-09-02 02:35:55 +0900

    editとdestroyを作成

--- app/controllers/article_page_controller.rb
+++ app/controllers/article_page_controller.rb
@@ -17,13 +17,32 @@ class ArticlePageController < ApplicationController
 
     @article = ArticlePage.new(article_params.merge(tag_list: tag_list_array))
     if @article.save
-      redirect_to @article, notice: 'Article was successfully created.'
+      redirect_to @article
     else
       render :new
     end
   end
 
   def edit
+    @article = ArticlePage.find(params[:id])
+  end
+
+  def update
+    @article = ArticlePage.find(params[:id])
+    tag_list_param = params[:article_page][:tag_list]
+    tag_list_array = tag_list_param.split(',')
+
+    if @article.update(article_params.merge(tag_list: tag_list_array))
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = ArticlePage.find(params[:id])
+    @article.destroy
+    redirect_to article_page_index_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :article_page, only: [:index, :show, :new, :edit, :update, :destroy]
+  resources :article_page, only: [:index, :show, :new, :edit, :update]
 
   post 'article_pages', to: 'article_page#create'
+  get 'article_page/delete/:id', to: 'article_page#destroy', as: 'delete_article_page'
   root "article_page#index"
 end
