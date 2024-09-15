9c69190f20cf90f96f08045ab2256b762405eb3f
haruki1227041209 <haruki122704@gmail.com>
2024-09-15 23:52:46 +0900

    user modelの作成と変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_01_074756) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_15_144754) do
   create_table "article_pages", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.text "body"
     t.text "tag_list"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
